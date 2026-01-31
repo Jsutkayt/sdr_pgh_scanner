5c3027bb7baf4553d34aaa0b9cc88b98c9ee8d25
Jackson Sutkaytis <165087602+Jsutkayt@users.noreply.github.com>
2026-01-31 15:29:48 -0500

    Delete bot_and_twitter/test.py

--- bot_and_twitter/test.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv("/Users/jacksonsutkaytis/Documents/Pgh_Scanner_Project/sdr_pgh_scanner/.env")
-
-print("XCONSUMER_KEY =", os.getenv("XCONSUMER_KEY"))
-print("XCONSUMER_KEY_SECRET =", os.getenv("XCONSUMER_SECRET"))
-print("XACCESS_KEY =", os.getenv("XACCESS_TOKEN"))
-print("XACCESS_TOKEN_SECRET =", os.getenv("XACCESS_SECRET"))
